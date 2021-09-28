227e27de17aa49648b95c5346e1604834aaf7617
eduardocampo <contactcampo@gmail.com>
2021-09-28 17:07:27 -0400

    Add: Absolute import components and lib

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import useUser from '../lib/Hooks/useUser'
+import useUser from '@/lib/Hooks/useUser'
 import Link from 'next/link'
 
 export default function Home() {
--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import useUser from '../../lib/Hooks/useUser'
+import useUser from '@/lib/Hooks/useUser'
 
 const Profile = () => {
   const {
