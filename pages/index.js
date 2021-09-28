4141291e65144197209ff4aad017dce73d9b1a7f
eduardocampo <contactcampo@gmail.com>
2021-09-28 16:50:43 -0400

    Create: SWR Hooks User from API

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import useUser from '../lib/Hooks/useUser'
+import Link from 'next/link'
+
 export default function Home() {
-	return (
-		<div>
-			<h1>Hello</h1>
-		</div>
-	)
+  const { user, isLoading, isError } = useUser()
+
+  if (isError) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  console.log('data', user || [])
+  return (
+    <div>
+      <h1>User</h1>
+      <div>
+        {user.data.map((item) => (
+          <div key={item.id}>
+            <div>{item.firstName}</div>
+            <Link href={{ pathname: '/user/[id]', query: { id: item.id } }}>
+              <a>
+                {item.firstName} {item.lastName}
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
